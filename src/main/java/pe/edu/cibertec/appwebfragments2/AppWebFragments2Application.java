a84a3e41a0f30c69de7ae06d2cd87a820527496e
julioMoscol <juliomoscolgonzales@gmail.com>
2023-10-01 15:24:58 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebfragments2/AppWebFragments2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragments2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFragments2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFragments2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebfragments2/AppWebFragments2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebfragments2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragments2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
